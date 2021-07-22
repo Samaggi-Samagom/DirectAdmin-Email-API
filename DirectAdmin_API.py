71aee84e7c09e4d9d66374a795d3c026f2dab9f0
BuildTools <unconfigured@null.spigotmc.org>
2021-07-22 22:00:36 +0700

    Python API for MXRoute DirectAdmin TODO: Add function for changing a user's quota
    Minimal testing done, requires more testing.

---
+++ DirectAdmin_API.py
@@ -0,0 +1,252 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import warnings
+import urllib.parse
+
+
+class DirectAdminResponse:
+
+    class Unknown:
+
+        def __init__(self, content: str, is_failure: bool = False):
+            self.content = content
+            self.failure = is_failure
+
+        def raw(self):
+            return self.content
+
+        def decode(self, *args):
+            return self.content
+
+        def is_error(self):
+            return "error" in self.content
+
+    class Text(Unknown):
+
+        def __init__(self, content: str, is_failure: bool = False):
+            super().__init__(content, is_failure)
+
+    class Location(Unknown):
+
+        def __init__(self, content: str, is_failure: bool = False):
+            super().__init__(content, is_failure)
+
+    class URLEncodedArray(Unknown):
+
+        def __init__(self, content: str, is_failure: bool = False):
+            super().__init__(content, is_failure)
+
+        def decode(self, key: str = "list"):
+            split = self.content.split(key + "[]=")
+            return [(x if x[-1] != "&" else x[:-1]) for x in split if x != ""]
+
+    class URLEncodedString(Unknown):
+
+        def __init__(self, content: str, is_failure: bool = False):
+            super().__init__(content, is_failure)
+
+        def decode(self, raw: bool = False):
+            res = {}
+            pairs = self.content.split("&")
+            for pair in pairs:
+                key, value = pair.split("=")
+                res[urllib.parse.unquote(key)] = urllib.parse.unquote(value) if raw else urllib.parse.unquote(value).split(",")
+            return res
+
+        def is_error(self):
+            response = self.decode()
+            if "error" not in list(response.keys()):
+                warnings.warn("Reply does not indicate success nor failure")
+                warnings.warn(str(response))
+                return False
+            else:
+                if response["error"][0] == "0":
+                    return False
+                else:
+                    return True
+
+
+class DirectAdmin:
+
+    def __init__(self, server: str, username: str, password: str, domain: str):
+        self.server = server
+        self.__user = username
+        self.__password = password
+        self.domain = domain
+        self.__check_domain()
+
+        self.__users_cache = []
+        self.__users_cache_valid = False
+
+    def __check_domain(self):
+        response = self.send_request("CMD_API_SHOW_DOMAINS", response_type=DirectAdminResponse.URLEncodedArray, failure_response_type=DirectAdminResponse.Text)
+        domains = response.decode()
+        if self.domain not in domains:
+            raise RuntimeError("The domain provided does not exist or you don't have permission to access it")
+
+    def send_request(self, function: str, payload: dict = None, response_type: DirectAdminResponse.__class__ = DirectAdminResponse.Unknown, failure_response_type: DirectAdminResponse.__class__ = DirectAdminResponse.Unknown):
+        response = requests.post(("https://" if "https://" not in self.server else "") + self.server + ("/" if self.server[-1] != "/" else "") + function, auth=HTTPBasicAuth(self.__user, self.__password), data=payload)
+
+        if response.status_code != 200:
+            warnings.warn("Server responded with a non-200 http code while trying to run function {function} on {server}. Response content : {content}".format(function=function, server=self.server, content=response.content))
+            return
+
+        direct_admin_response = response_type(response.text)
+        failure_response = failure_response_type(response.text, is_failure=True)
+
+        if failure_response.is_error():
+            return failure_response
+        return direct_admin_response
+
+    def create_user(self, username: str, password: str, quota: int):
+        payload = {"action": "create",
+                   "domain": self.domain,
+                   "quota": quota,
+                   "user": username,
+                   "passwd": password}
+
+        response = self.send_request("CMD_API_POP", payload, response_type=DirectAdminResponse.Text, failure_response_type=DirectAdminResponse.URLEncodedString)
+
+        if response.failure:
+            warnings.warn("Failed to create user")
+            warnings.warn(str(response.decode()))
+            return False
+        return True
+
+    def delete_user(self, username: str):
+        payload = {"action": "delete",
+                   "domain": self.domain,
+                   "user": username}
+
+        response = self.send_request("CMD_API_POP", payload, response_type=DirectAdminResponse.Text)
+
+        if response.failure:
+            warnings.warn("Failed to delete user")
+            warnings.warn(response.decode())
+            return False
+        return True
+
+    def list_users(self, cache: bool = False):
+        self.__users_cache_valid = False
+
+        payload = {"action": "list",
+                   "domain": self.domain}
+
+        response = self.send_request("CMD_API_POP", payload, response_type=DirectAdminResponse.URLEncodedArray)
+
+        if response.failure:
+            warnings.warn("Failed to list users")
+            print(response.decode())
+            return
+
+        if cache:
+            self.__users_cache = response.decode()
+            self.__users_cache_valid = True
+
+        return response.decode()
+
+    def user_exists(self, username: str):
+        existing_users = self.__users_cache
+        if not self.__users_cache_valid:
+            existing_users = self.list_users(cache=False)
+
+        return username in existing_users
+
+    def list_forwarders(self, raw: bool = False):
+        payload = {"action": "list",
+                   "domain": self.domain}
+
+        response = self.send_request("CMD_API_EMAIL_FORWARDERS", payload, response_type=DirectAdminResponse.URLEncodedString)
+
+        if response.failure:
+            warnings.warn("Failed to list forwarders")
+            warnings.warn(response.decode())
+            return
+
+        response.__class__ = DirectAdminResponse.URLEncodedString
+
+        return response.decode(raw)
+
+    def modify_forwarder_raw(self, forwarder: str, value: str):
+        payload = {"action": "modify",
+                   "domain": self.domain,
+                   "user": forwarder,
+                   "email": value}
+
+        response = self.send_request("CMD_API_EMAIL_FORWARDERS", payload, response_type=DirectAdminResponse.Text)
+
+        if response.failure:
+            warnings.warn("Failed to modify forwarder")
+            warnings.warn(response.decode())
+            return False
+        return True
+
+    def remove_user_forwarder(self, username: str, forwarder: str):
+        forwarders = self.list_forwarders()
+
+        if forwarder not in forwarders.keys():
+            warnings.warn("Forwarder list does not exist, skipping. Forwarder: " + forwarder)
+            return False
+
+        current_forwarder = forwarders[forwarder]
+
+        if username not in current_forwarder:
+            warnings.warn("The user requested does not exist in the forwarder list, skipping. Username: {username}, Forwarder: {forwarder}".format(username=username, forwarder=forwarder))
+
+        current_forwarder.remove(username)
+        new_forwarder = ",".join([u + "@" + self.domain for u in current_forwarder])
+
+        self.modify_forwarder_raw(forwarder, new_forwarder)
+
+    def add_user_forwarder(self, username: str, forwarder: str):
+        forwarders = self.list_forwarders()
+
+        if forwarder not in forwarders.keys():
+            warnings.warn("Forwarder list does not exist, skipping. Forwarder: " + forwarder)
+            return False
+
+        current_forwarder = forwarders[forwarder]
+
+        if username in current_forwarder:
+            warnings.warn("The user you're trying to add already exists in the forwarder list, skipping. Username: {username}, Forwarder: {forwarder}".format(username=username, forwarder=forwarder))
+            return False
+
+        current_forwarder.append(username)
+        new_forwarder = ",".join([u + "@" + self.domain for u in current_forwarder])
+
+        self.modify_forwarder_raw(forwarder, new_forwarder)
+
+    def set_users_forwarder(self, usernames: list, forwarder: str):
+        forwarders = self.list_forwarders()
+
+        if forwarder not in forwarders.keys():
+            warnings.warn("Forwarder list does not exist, creating. Forwarder: " + forwarder)
+            return self.create_forwarder(forwarder_name=forwarder, usernames=usernames)
+
+        new_forwarder = ",".join([u + "@" + self.domain for u in usernames])
+
+        self.modify_forwarder_raw(forwarder, new_forwarder)
+
+    def create_forwarder(self, forwarder_name: str, username: str = None, usernames: list = None):
+        if username is None and usernames is None:
+            raise RuntimeError("create_forwarder requires either username: str or usernames: list as a parameter. None given.")
+        if username is not None and usernames is not None:
+            warnings.warn("create_forwarder given both username: str and usernames: list, will use usernames: list")
+        elif username is not None and usernames is None:
+            usernames = [username]
+
+        forwarder_value = ",".join([u + "@" + self.domain for u in usernames])
+
+        payload = {"action": "create",
+                   "domain": self.domain,
+                   "user": forwarder_name,
+                   "email": forwarder_value}
+
+        response = self.send_request("CMD_API_EMAIL_FORWARDERS", payload, response_type=DirectAdminResponse.Text, failure_response_type=DirectAdminResponse.URLEncodedString)
+
+        if response.failure:
+            warnings.warn("Failed to create forwarder")
+            warnings.warn(response.decode())
+            return False
+        return True
+
---
+++ README.md
@@ -0,0 +1,192 @@
+# Python API for Managing MXRoute Email Accounts through DirectAdmin API
+
+##Requirements
+
+### Python
+Tested on Python 3.7. Does not support Python 2 due to the usage of `urllib.parse`.
+
+### Packages
+- `requests` (and its dependencies)
+
+### Access
+Requires access to the following commands on DirectAccess  
+-`CMD_API_SHOW_DOMAINS` (required for all use)  
+-`CMD_API_EMAIL_FORWARDERS` (required for creating/modifying email forward list)  
+-`CMD_API_POP` (required for creating/modifying/deleting email account)
+ 
+
+##Initialisation
+**my_code.py** (Your main Python file):
+```
+from DirectAdmin_API import DirectAdmin
+from credentials import server, username, password, domain
+
+manager = DirectAdmin(server=server, username=username, password=password, domain=domain)
+``` 
+
+**credentials.py** (In the same folder as your Python file)
+```
+server = something.mxrouting.net:2222       #The same url you use to log in to console
+username = admin                            #The username used to sign in
+password = myPassword                       #The password used to sign in
+domain = mydomain.com                       #The domain that you would like to edit
+```
+
+##Documentation
+
+
+###Custom API Request
+`.checkRequest(function: str, payload: dict, response_type: DirectAdminResponse.__class__, failure_response_type: DirectAdminResponse.__class__) -> DirectAdminResponse`
+
+
+>**function:** function that you would like to call. See the API guide [here](https://www.directadmin.com/api.php)  
+>**payload:** dictionary data for the POST request, consult API guide  
+>**response_type:** see DirectAdminResponseType section below  
+>**failure_response_type:** see DirectAdminResponseType section below
+
+Returns DirectAdmin Response Object.  
+Query `.failure` on the return value to check if it is the success response or a failure response  
+Call `.decode()` on the return value to get the decoded response  
+Call `.raw()` on the return value to get the raw string response
+
+<br/>
+
+###Creating an Email Account
+`.create_user(username: str, password: str, quota: int) -> bool`
+
+>**username:** username of the user to be created (do NOT include @domain.com)  
+>**password:** password of the user to be created (Must have one number and one upper-cased letter)
+>**quota:** email storage quota in MB
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###Deleting an Email Account 
+`.delete_user(username: str)`
+
+>**username:** username of the user to be deleted
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###List Users
+`.list_users(cache: bool) -> list`
+
+>**cache:** temporarily keeps the list of users in memory for use in `.user_exists()`. Should be kept False otherwise. Cache is cleared if `.list_users()` is called again with `cache=False`
+
+Returns list of string.  
+Contains usernames of all email users (does not include @domain.com)
+
+<br/>
+
+###Check if User Exists
+
+`.user_exists(username: str) -> bool`
+
+>**username:** username of the user to be checked
+
+Returns boolean.  
+**TRUE** if username exists 
+**FALSE** if username does not exist
+
+<br/>
+
+###List All Forwarding Lists
+
+`.list_forwarders(raw: bool) -> dict`
+
+>**raw:** should the code return the usernames in each forward list raw (as a comma-separated string) or as an array. Defaults to False
+
+Returns dictionary.  
+If `raw=True`, the dictionary will be in the following format `{string: string}`. The value of each key will be a comma-separated string containing all usernames that the key  will forward to.  
+If `raw=False`, the dictionary will be in the following format `{string: [string]}`. The value of each key will be an array of string containing all usernames that the key will forward to it.
+
+<br/>
+
+###Creating a Forwarding List
+
+`.create_forwarder(forwarder_name: str, username: str, usernames: list) -> bool`
+
+>**forwarder_name:** the forwarder name  
+> **AND EITHER**  
+> **username:** A string containing the (singular) user that the forwarder should forward to  
+> **OR**  
+> **usernames:** A list of string containing the users that the forwarder should forward to
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###Set Forwarding List (Raw Value)
+This is used to completely replace an existing forwarding list. The current value for the forwarding list will be replaced.
+
+
+`.modify_forwarder_raw(forwarder: str, value: str) -> bool`
+
+>**forwarder:** the forwarder name  
+>**value:** comma-separated string of all emails (**including** @domain.com)  
+>Emails sent to **forwarder** will be forwarded to the users listed in **value**
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###Set Forwarding List
+This is used to completely replace an existing forwarding list. The current value for the forwarding list will be replaced.
+
+`.set_users_forwarder(usernames: list, forwarder: str) -> bool`
+
+>**usernames:** a list of string containing the usernames of the users which the email sent to the forwarder should be forwarded to (do not include @domain.com)  
+>**forwarder:** the forwarder name
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###Add User to Forwarding List
+
+`.add_user_forwarder(username: str, forwarder: str) --> bool`
+
+>**username:** username of the username to be removed from the forwarder list (do not include @domain.com)
+>**forwarder:** the forwarder name
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+<br/>
+
+###Remove User from Forwarding List
+
+`.remove_user_forwarder(username: str, forwarder: str) -> bool`
+
+>**username:** username of the username to be removed from the forwarder list (not including @domain.com)
+>**forwarder:** the forwarder name
+
+Returns boolean.  
+**TRUE** if operation was successful  
+**FALSE** if operation failed. Will print warning with details
+
+
+###DirectAdminResponseType 
+
+When making a custom API request, the correct response type must be passed to the function to ensure that the response can be understood correctly. For most API calls, the expected response type is provided in the API guide.
+
+```
+DirectAdminResponseType.Unknown             #Assumes string
+DirectAdminResponseType.Text                #Treats the response as a string
+DirectAdminResponseType.URLEncodedString    #Treats the response as a URL, trys to generate a dictionary (e.g. error=0&name=pakkapol)
+DirectAdminResponseType.URLEncodedArray(key)#Treats the response as an array embedded in a url (PHP Style) (e.g. list[]=item1&list[]=item2) custom key can be provided (defaults to "list")
+```
